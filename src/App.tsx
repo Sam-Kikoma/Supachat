cab787a8a4e033c0910e99bf6b7a3872931587e4
Sam Kikoma <kikomasamson@gmail.com>
2025-06-30 22:25:21 +0200

    feat: restructure App and main components for routing and context integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import Navbar from "./components/Navbar";
+
 const App = () => {
-	return <div>App</div>;
+	return (
+		<div className="p-4">
+			<Navbar />
+			<h1>Stuff</h1>
+		</div>
+	);
 };
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import App from "./App.tsx";
+import { AuthProvider } from "./context/AuthContext.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const queryClient = new QueryClient();
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />,
+	},
+]);
+
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<QueryClientProvider client={queryClient}>
+			<AuthProvider>
+				<RouterProvider router={router} />
+			</AuthProvider>
+		</QueryClientProvider>
+	</StrictMode>
+);
