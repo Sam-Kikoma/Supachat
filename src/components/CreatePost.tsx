3f7c6d3139b5821314ae7ef23fddc4f5f23a0e85
Sam Kikoma <kikomasamson@gmail.com>
2025-07-08 22:20:11 +0200

    feat: post creation functionality added

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -1,18 +1,65 @@
 import { useState } from "react";
+import { useMutation } from "@tanstack/react-query";
+import supabase from "../utils/supabase";
+
+interface PostInput {
+	title: string;
+	content: string;
+}
+
+const createPost = async (post: PostInput) => {
+	const { data, error } = await supabase.from("posts").insert(post);
+	if (error) throw new Error(error.message);
+	return data;
+};
 
 const CreatePost = () => {
 	const [title, setTitle] = useState<string>("");
 	const [content, setContent] = useState<string>("");
+
+	const { mutate } = useMutation({
+		mutationFn: createPost,
+		onSuccess: () => {
+			setTitle("");
+			setContent("");
+		},
+	});
+
+	const handleSubmit = (event: React.FormEvent) => {
+		event.preventDefault();
+		mutate({ title, content });
+		setTitle("");
+		setContent("");
+	};
+
 	return (
 		<div>
-			<form action="">
+			<form onSubmit={handleSubmit}>
 				<div>
-					<label htmlFor="title">Title</label>
-					<input type="text" id="title" required onChange={(event) => setTitle(event.target.value)} />
+					<label className="block" htmlFor="title">
+						Title
+					</label>
+					<input
+						className="input block"
+						type="text"
+						id="title"
+						required
+						onChange={(event) => setTitle(event.target.value)}
+						value={title}
+					/>
 				</div>
 				<div>
-					<label htmlFor="content">Content</label>
-					<textarea id="content" required rows={5} onChange={(event) => setContent(event.target.value)} />
+					<label htmlFor="content" className="block">
+						Content
+					</label>
+					<textarea
+						className="textarea block"
+						id="content"
+						required
+						rows={5}
+						onChange={(event) => setContent(event.target.value)}
+						value={content}
+					/>
 				</div>
 				<button className="btn btn-soft btn-info" type="submit">
 					Create Post
--- src/pages/CreatePostPage.tsx
+++ src/pages/CreatePostPage.tsx
@@ -1,7 +1,10 @@
+import CreatePost from "../components/CreatePost";
+
 const CreatePostPage = () => {
 	return (
-		<div>
-			<h2>Create New Post</h2>
+		<div className="max-w-[350px] border-2 border-amber-100 flex flex-col p-2 rounded-lg">
+			<h2 className="block">New Post</h2>
+			<CreatePost />
 		</div>
 	);
 };
