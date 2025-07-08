bf7a771afaf75de33f0a935263ff4a8c58779c37
Sam Kikoma <kikomasamson@gmail.com>
2025-07-08 22:19:50 +0200

    feat:create post

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { Outlet } from "react-router";
 import Navbar from "./components/Navbar";
 
 const App = () => {
 	return (
 		<div className="p-4">
 			<Navbar />
-			<h1>Stuff</h1>
+			<div className="py-4">
+				<Outlet />
+			</div>
 		</div>
 	);
 };
